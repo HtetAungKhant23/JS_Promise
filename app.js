adeec067dd759791305ece61d9d6036935a25a94
htetaungkhant <htetaung23.ha@gmail.com>
2023-05-22 12:33:47 +0630

    promise changing understanding and testing

--- app.js
+++ app.js
@@ -31,22 +31,96 @@ const fetchData = () => {
     });
 }
 
+const getUserPhoto = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('getting user photo...');
+        }, 3000);
+    });
+}
+
+const frindAdd = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('frind adding....');
+        }, 3000);
+    })
+}
+
+const serching = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('searching...');
+        }, 3000);
+    })
+}
+
 const homeFeed = (data) => {
     console.log(data);
 }
 
+
+// Promise changing
 login()
-    .then(logindata => {
-        console.log(logindata);
-        fetchData()
-            .then(fetchdata => {
-                console.log(fetchdata);
-                homeFeed('this is home feed with data from fetch!');
-            })
-            .catch(err => {
-                console.log(err);
-            })
+    .then(firstData => {
+        console.log(firstData);
+        return fetchData();
+    })
+    .then(secondData => {
+        console.log(secondData);
+        return getUserPhoto();
+    })
+    .then(thridData => {
+        console.log(thridData);
+        return frindAdd();
+    })
+    .then(fourthData => {
+        console.log(fourthData);
+        return serching();
+    })
+    .then(final => {
+        console.log(final);
+        homeFeed('this is final home feed');
     })
     .catch(err => {
         console.log(err);
     })
+
+
+// callback hell (or) pyramid dom
+// login()
+//     .then(logindata => {
+//         console.log(logindata);
+//         fetchData()
+//             .then(fetchdata => {
+//                 console.log(fetchdata);
+//                 getUserPhoto()
+//                     .then(data => {
+//                         console.log(data);
+//                         frindAdd()
+//                             .then(frindData => {
+//                                 console.log(frindData);
+//                                 serching()
+//                                     .then(serchData => {
+//                                         console.log(serchData);
+//                                         homeFeed('this is final home feed');
+//                                     })
+//                                     .catch(err => {
+//                                         console.log(err);
+//                                     })
+//                             })
+//                             .catch(err => {
+//                                 console.log(err);
+//                             })
+//                     })
+//                     .catch(err => {
+//                         console.log(err);
+//                     })
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//             })
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
