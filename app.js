ea9e295a3f5723dabe223913ecf55ec678d80b43
htetaungkhant <htetaung23.ha@gmail.com>
2023-05-22 14:41:03 +0630

    fetch api using async await

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const fetchData = () => {
 const getUserPhoto = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('getting user photo...');
+            // resolve('getting user photo...');
+            reject('this is error!');
         }, 3000);
     });
 }
@@ -58,6 +59,53 @@ const serching = () => {
 const homeFeed = (data) => {
     console.log(data);
 }
+
+
+async / await
+
+const testOne = async () => {
+    try{
+        const firstData = await login();
+        console.log(firstData);
+        const secondData = await fetchData();
+        console.log(secondData);
+        const thridData = await getUserPhoto();
+        console.log(thridData);
+        const fourthData = await frindAdd();
+        console.log(fourthData);
+        const finalData = await serching();
+        console.log(finalData);
+        homeFeed('this is final');
+    }catch(error){
+        console.log(error);
+    }
+}
+
+console.log('first');
+testOne();
+console.log('second');
+
+const testTwo = async () => {
+    const firstData = await login();
+    console.log(firstData);
+    const secondData = await fetchData();
+    console.log(secondData);
+    const thridData = await getUserPhoto();
+    console.log(thridData);
+    const fourthData = await frindAdd();
+    console.log(fourthData);
+    const finalData = await serching();
+    console.log(finalData);
+    homeFeed('this is final');
+}
+
+console.log('first');
+testTwo().catch(error => {
+    console.log(error);
+});
+console.log('second');
+
+
 
 
 // Promise changing
@@ -88,39 +136,70 @@ login()
 
 
 // callback hell (or) pyramid dom
-// login()
-//     .then(logindata => {
-//         console.log(logindata);
-//         fetchData()
-//             .then(fetchdata => {
-//                 console.log(fetchdata);
-//                 getUserPhoto()
-//                     .then(data => {
-//                         console.log(data);
-//                         frindAdd()
-//                             .then(frindData => {
-//                                 console.log(frindData);
-//                                 serching()
-//                                     .then(serchData => {
-//                                         console.log(serchData);
-//                                         homeFeed('this is final home feed');
-//                                     })
-//                                     .catch(err => {
-//                                         console.log(err);
-//                                     })
-//                             })
-//                             .catch(err => {
-//                                 console.log(err);
-//                             })
-//                     })
-//                     .catch(err => {
-//                         console.log(err);
-//                     })
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//             })
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
+login()
+    .then(logindata => {
+        console.log(logindata);
+        fetchData()
+            .then(fetchdata => {
+                console.log(fetchdata);
+                getUserPhoto()
+                    .then(data => {
+                        console.log(data);
+                        frindAdd()
+                            .then(frindData => {
+                                console.log(frindData);
+                                serching()
+                                    .then(serchData => {
+                                        console.log(serchData);
+                                        homeFeed('this is final home feed');
+                                    })
+                                    .catch(err => {
+                                        console.log(err);
+                                    })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            })
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
+
+// fetch API / promise changing
+fetch("https://fakestoreapi.com/products")
+    .then(result => {
+        return result.json();
+    })
+    .then(products => {
+        console.log(products);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+// async/await  Fetch API
+const test = async () => {
+    try{
+        const data = await fetch("https://fakestoreapi.com/products");
+        const products = await data.json();
+        console.log(products);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+console.log('frist');
+test();
+console.log('second');
